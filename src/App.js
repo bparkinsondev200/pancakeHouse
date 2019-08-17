818342567412e9e50bf3c2481a1e30eeb635d09d
Brayden Parkinson <brayden.parkinson@aruplab.com>
2019-08-17 17:25:18 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Dropdown, Segment, Header, Input } from 'semantic-ui-react';
+import { flipPancakes, flipTheCakes } from './flipCakes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    pancakes: [],
+    theCakes: [],
+    numberOfRandomCases: 1,
+    manualTestCases: []
+  }
+
+  render() {
+
+    const setCakes = () => {
+      this.setState({
+        pancakes: flipTheCakes(this.state.numberOfRandomCases, this.state.manualTestCases)
+      })
+    }
+
+    const setNumRandomCases = (e) => {
+      let val = parseInt(e.target.innerText)
+      this.setState({
+        numberOfRandomCases: val
+      })
+    }
+
+    const setManualTestCases = (e) => {
+      let val = e.target.value
+      this.setState({
+        manualTestCases: [val]
+      })
+    }
+
+    const options = () => {
+      let optionsArr = []
+      for (let i = 0; i <= 100; i++) {
+        let newObj = {
+          key: i,
+          text: i,
+          value: i
+        }
+        optionsArr.push(newObj)
+      }
+      return optionsArr
+    }
+
+    const styles = {
+      segmentStyle: {
+        width: '50vw',
+        margin: 'auto',
+        top: 250
+      }
+    }
+
+    return (
+      <div className="App">
+        <Segment style={styles.segmentStyle}>
+          <Header style={{textAlign: 'center'}}>
+            Happy Pancake House
+          </Header>
+          <h5>
+            Select how many pancake stacks to generate. Each stack will be randomly generated.
+          </h5>
+          <Dropdown
+            selection
+            placeholder='Select Random Stack Amount'
+            options={options()}
+            onChange={(e) => setNumRandomCases(e)}
+          >
+          </Dropdown>
+          <h3>
+            OR Enter your own
+          </h3>
+          <Input onBlur={(e) => setManualTestCases(e)}/>
+          <br/>
+          <Button size='large' style={{margin: 25}}onClick={() => setCakes()}>
+            Click to flip the Cakes!
+          </Button>
+
+          <div>
+            {this.state.pancakes.map((element) => {
+              return (
+                <div>
+                  {element}
+                </div>
+              )
+            })}
+          </div>
+        </Segment>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/flipCakes.js
@@ -0,0 +1,78 @@
+export const flipTheCakes = (numOfTestCases, manualTestCaseEntry = []) => {
+  /*First set the amount of test cases*/
+  const T = numOfTestCases
+
+  /*Then generate an array of test strings, with T limiting
+  the size of the array*/
+  const generateTestCases = (numOfTests) => {
+    let pancakeArr = [];
+    let pancakeElement;
+    /*Loop the test num of cases*/
+    for (let x = 0; x < numOfTests; x++) {
+      /*Based on the current test case num,
+      create a string with that length,
+      add to locally scoped pancakeElement
+      then push to functionially scoped pancakeArr*/
+      pancakeElement = ''
+      let S = 0
+      do {
+        S++
+        pancakeElement += Math.round(Math.random()) ? '+' : '-';
+      } while (S <= x);
+      pancakeArr.push(pancakeElement);
+    }
+    return pancakeArr;
+  };
+
+  let S = manualTestCaseEntry.length && manualTestCaseEntry[0] !== '' ? manualTestCaseEntry : generateTestCases(T)
+
+  //flipperFunction to take how many to flip, and the pancake stack being flipped
+  const flipCakes = (numOfCakes, cakeArray) => {
+    let activeVal = cakeArray[numOfCakes - 1]
+    cakeArray.splice(0, numOfCakes)
+    for (let x = 0; x < numOfCakes; x++) {
+      cakeArray.unshift(activeVal === '-' ? '+' : '-')
+    }
+    return cakeArray
+  }
+
+  //declare object where the keys are the pancakes strings and the value is the numTimesFlipped
+  let pancakesFlipped = []
+  // loop through the list of testCases
+  for (let x = 0; x < S.length; x++) {
+    // for every string, split into array that will be later joined
+    let numPancakesToBeFlipped = 0
+    let flipCounter = 0
+    let currentStringArr = S[x].split('')
+    // loop through the string array while the number of pancakes to be flipped is less than or equal to the length of the string
+    // AND while the string contains no '-'
+    for (let i = 0; i < currentStringArr.length; i++) {
+      // if the current string is the same as the next string, continue and add a counter for how many pancakes will be flipped
+      if (currentStringArr[i] === currentStringArr[(i + 1)]) {
+        numPancakesToBeFlipped++
+      }
+      else if (!currentStringArr.includes('-')) {
+        break;
+      }
+      // if the current string is not the same as the next string, flip the current number of pancakes to be flipped
+      else {
+        numPancakesToBeFlipped++
+        currentStringArr = flipCakes(numPancakesToBeFlipped, currentStringArr)
+        // when pancakes are flipped, add to a flipCounter variable for each
+        flipCounter++
+        pancakesFlipped[x] = [currentStringArr.join(''), flipCounter]
+      }
+    }
+    // if pancakesToBeFlipped is equal to length of string, and contains '-', flip one more time
+    if (numPancakesToBeFlipped === currentStringArr.length && currentStringArr.includes('-')) {
+      currentStringArr = flipCakes(currentStringArr.length, currentStringArr)
+    }
+    // otherwise join that array back up, and add it to a variable keeping track of the corrected pancakes. 
+    currentStringArr.join('')
+    // now add the flippedPancake and number of times flipped to the return object
+    pancakesFlipped[x] = [S[x], flipCounter]
+    //loop should start over
+  }// end loop
+
+  return pancakesFlipped //return object
+}
